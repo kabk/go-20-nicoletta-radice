6fb78714fe495bf5596b627c5e7b804062dcc4c0
Nicoletta <54234635+NicolettaRadice@users.noreply.github.com>
2020-01-19 00:25:33 +0100

    updated 19January

--- script/script.js
+++ script/script.js
@@ -82,44 +82,25 @@ $(".clickable").on("click", actionLeft);
 $(".swipable2").on("swipe", actionRight);
 $(".clickable2").on("click", actionRight);
 
-//   $("#left").click(function(){
-//     if ( $(this).hasClass("isOpen") ) {
-//       $(this).animate({
-//         marginLeft: '0vw'
-//       }),
-//       $("#left-door").animate({
-//           width: '20px'
-//       });
-//     } else {
-//       $(this).animate({
-//         marginLeft: '75vw'
-//       }),
-//       $("#left-door").animate({
-//           width: '80%'
-//       });
-//     }
-//     $(this).toggleClass("isOpen");
-// });
-//
-//
-// $("#right").click(function(){
-//   if ( $(this).hasClass("isOpen") ) {
-//     $(this).animate({
-//       left: '85%'
-//     }),
-//     $("#right-door").animate({
-//         width: '20px'
-//     });
-//   } else {
-//     $(this).animate({
-//       left: '10vw'
-//     }),
-//     $("#right-door").animate({
-//         width: '80%'
-//     });
-//   }
-//   $(this).toggleClass("isOpen");
-// });
+$(function() {
+    // Change this selector to find whatever your 'boxes' are
+  var top = $(".panel-container");
+    // Set up click handlers for each box
+    top.click(function() {
+        var el = $(this), // The box that was clicked
+            max = 0;
+        // Find the highest z-index
+        top.each(function() {
+            // Find the current z-index value
+            var z = parseInt( $( this ).css( "z-index" ), 10 );
+            // Keep either the current max, or the current z-index, whichever is higher
+            max = Math.max( max, z );
+        });
+
+        // Set the box that was clicked to the highest z-index plus one
+        el.css("z-index", max + 10 );
+    });
+});
 
 $('#triggervideo').click(function(){
   $('video').trigger('play');
