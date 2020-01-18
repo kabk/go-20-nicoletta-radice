0750530501ef79b279f07501803eed3da1b4c570
Nicoletta <54234635+NicolettaRadice@users.noreply.github.com>
2020-01-18 14:27:14 +0100

    mobile test swipe action

--- script.js
+++ script.js
@@ -17,15 +17,106 @@ $(".trigger").click(function(){
   });
 
 
-  $(".sliding-panel").click(function(){
-    if ( $(this).hasClass("isOpen") ) {
-      $(this).animate({
-        width: '20px'
+//   $(".sliding-panel").click(function(){
+//     if ( $(this).hasClass("isOpen") ) {
+//       $(this).animate({
+//         width: '20px'
+//       }),
+//       $(".circle").animate({
+//         marginLeft: '0vw'
+//       });
+//     } else {
+//       $(this).animate({
+//         width: '80%'
+//       }),
+//       $(".circle").animate({
+//         marginLeft: '75vw'
+//       });
+//     }
+//     $(this).toggleClass("isOpen");
+// });
+
+
+
+function actionLeft() {
+  if ( $("#left").hasClass("isOpen") ) {
+        $("#left").animate({
+          marginLeft: '0vw'
+        }),
+        $("#left-door").animate({
+            width: '20px'
+        });
+      } else {
+        $("#left").animate({
+          marginLeft: '75vw'
+        }),
+        $("#left-door").animate({
+            width: '80%'
+        });
+      }
+      $(this).toggleClass("isOpen");
+}
+
+function actionRight() {
+    if ( $("#right").hasClass("isOpen") ) {
+      $("#right").animate({
+        left: '85%'
+      }),
+      $("#right-door").animate({
+          width: '20px'
       });
     } else {
-      $(this).animate({
-        width: '80%'
+      $("#right").animate({
+        left: '10vw'
+      }),
+      $("#right-door").animate({
+          width: '80%'
       });
     }
-    $(this).toggleClass("isOpen");
-  });
+    $("#right").toggleClass("isOpen");
+}
+
+$(".swipable").on("swipe", actionLeft);
+$(".clickable").on("click", actionLeft);
+
+$(".swipable2").on("swipe", actionRight);
+$(".clickable2").on("click", actionRight);
+
+//   $("#left").click(function(){
+//     if ( $(this).hasClass("isOpen") ) {
+//       $(this).animate({
+//         marginLeft: '0vw'
+//       }),
+//       $("#left-door").animate({
+//           width: '20px'
+//       });
+//     } else {
+//       $(this).animate({
+//         marginLeft: '75vw'
+//       }),
+//       $("#left-door").animate({
+//           width: '80%'
+//       });
+//     }
+//     $(this).toggleClass("isOpen");
+// });
+//
+//
+// $("#right").click(function(){
+//   if ( $(this).hasClass("isOpen") ) {
+//     $(this).animate({
+//       left: '85%'
+//     }),
+//     $("#right-door").animate({
+//         width: '20px'
+//     });
+//   } else {
+//     $(this).animate({
+//       left: '10vw'
+//     }),
+//     $("#right-door").animate({
+//         width: '80%'
+//     });
+//   }
+//   $(this).toggleClass("isOpen");
+// });
